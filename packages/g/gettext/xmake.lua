97d7e5f04e46cf5ff29c23a2bb4cf1f41d1afce4
Saikari <lin@sz.cn.eu.org>
2025-05-02 01:35:12 +0300

    Update xmake.lua

--- packages/g/gettext/xmake.lua
+++ packages/g/gettext/xmake.lua
@@ -90,6 +90,9 @@ package("gettext")
             end
         end
         if is_subhost("windows") then
+            for _, file in ipairs(os.files("**")) do
+                io.replace(file, [[LIBTOOL='$(SHELL) $(top_builddir)/libtool']], [[LIBTOOL='"$(SHELL)" "$(top_builddir)/libtool"']], {plain = true})
+            end
             for _, file in ipairs(os.files("**.in")) do
                 io.replace(file, [[$(SHELL)]], [["$(SHELL)"]], {plain = true})
             end
