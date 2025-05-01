d95e232f8ae2f86153aea1fa468e05b909b6aa3d
Saikari <lin@sz.cn.eu.org>
2025-05-02 00:06:15 +0300

    Test

--- packages/g/gettext/xmake.lua
+++ packages/g/gettext/xmake.lua
@@ -89,6 +89,12 @@ package("gettext")
                 end
             end
         end
+        if is_subhost("windows") then
+            for _, file in ipairs(os.files("**")) do
+                io.replace(file, [[LIBTOOL='$(SHELL) $(top_builddir)/libtool']], [[LIBTOOL='"$(SHELL)" "$(top_builddir)/libtool"']], {plain = true})
+                io.replace(file, [[$(SHELL) $(top_srcdir)]], [["$(SHELL)" $(top_srcdir)]], {plain = true})
+            end
+        end
         import("package.tools.autoconf").install(package, configs, {cflags = cflags, ldflags = ldflags})
         package:addenv("PATH", "bin")
     end)
